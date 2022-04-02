a56a17f3ab8d6be01e114fbbc56ecc246274a75a
Alex2055 <alexfromnashville@gmail.com>
2022-04-02 18:15:25 -0500

    locators fixed us8

--- src/test/java/com/tryCloud/pages/US8Pages.java
+++ src/test/java/com/tryCloud/pages/US8Pages.java
@@ -11,10 +11,10 @@ public class US8Pages extends BasePage {
     @FindBy(xpath ="//*[@id=\"appmenu\"]/li[2]")
     public WebElement filesButton;
 
-    @FindBy(xpath = "//*[@id=\"fileList\"]/tr[1]/td[2]/a/span[2]/a[2]")
+    @FindBy(xpath = "//tbody[@id='fileList']/tr[2]/td[2]//a[2]")
     public WebElement actionButton;
 
-    @FindBy(xpath = "//*[@id=\"fileList\"]/tr[1]/td[2]/div/ul/li[9]/a")
+    @FindBy(xpath = "//span[text()='Delete file']/..")
     public WebElement deleteButton;
 
     @FindBy (xpath =" //*[@id=\"app-navigation\"]/ul/li[7]/a" )
