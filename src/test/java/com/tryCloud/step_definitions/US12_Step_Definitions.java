c8cf8ddf093d47c4dc9a06100ad3ee84505c13f8
ekelleci <ekelleci@outlook.com>
2022-04-02 12:37:50 -0400

    kelleciday1

---
+++ src/test/java/com/tryCloud/step_definitions/US12_Step_Definitions.java
@@ -0,0 +1,6 @@
+package com.tryCloud.step_definitions;
+
+public class US12_Step_Definitions {
+
+
+}
---
+++ src/test/java/com/tryCloud/step_definitions/US8_Step_Definitions.java
@@ -0,0 +1,29 @@
+package com.tryCloud.step_definitions;
+
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+    public class US8_Step_Definitions {
+    @Given ("user on the dashboard page")
+    public void user_on_the_dashboard_page(){
+    }
+
+    @And("user click action-icon from any file on the page")
+    public void userClickActionIconFromAnyFileOnThePage() {
+    }
+
+    @And("user choose the {string} option")
+    public void userChooseTheOption(String arg0) {
+    }
+
+    @When("the user clicks the {string} sub-module on the left side")
+    public void theUserClicksTheSubModuleOnTheLeftSide(String arg0) {
+    }
+
+    @Then("Verify the deleted file is displayed on the page.")
+    public void verifyTheDeletedFileIsDisplayedOnThePage() {
+    }
+}
