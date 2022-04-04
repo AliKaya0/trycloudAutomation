3a0b442619428841deda7493b6c1d7ffe3630653
elenafarima <farimaelena@yahoo.com>
2022-04-03 21:35:03 -0400

    US2 Done

---
+++ src/test/java/com/tryCloud/pages/LoginFailPage.java
@@ -0,0 +1,27 @@
+package com.tryCloud.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginFailPage extends BasePage{
+
+
+
+    @FindBy(id = "user")
+    public WebElement login;
+
+    @FindBy(id = "password")
+    public WebElement password;
+
+    @FindBy(id = "submit-form")
+    public WebElement loginButton;
+
+    @FindBy(css="[class*='warning']")
+    public WebElement message;
+
+
+
+
+
+
+}
